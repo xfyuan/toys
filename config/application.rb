56d7e74256de856ae2c3db3c660217fb492b999f
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-01-17 09:45:16 +0800

    config to not generate the files which will not be used

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module Toys
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
