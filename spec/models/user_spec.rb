f780a6bf58a304ad38457d162f7a04fa12ddba68
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-01-22 16:52:05 +0800

    add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :each do
+    @user = build :user
+  end
+
+  subject { @user }
+
+  it { should respond_to :email }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+  it { should be_valid }
+
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of :password }
+  it { should allow_value('example@domain.com').for(:email) }
+end
