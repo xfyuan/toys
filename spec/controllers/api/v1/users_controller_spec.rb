7c9a91e9a9b35a6d79df71c4d785a90d0bd89741
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-01-22 18:08:52 +0800

    add users#show and specs

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'Get #show' do
+    before :each do
+      @user = create :user
+      get :show, params: { id: @user.id }
+    end
+
+    it { should respond_with 200 }
+
+    it 'returns a user response' do
+      json_response = JSON.parse response.body, symbolize_names: true
+      expect(json_response[:email]).to eq @user.email
+    end
+  end
+end
