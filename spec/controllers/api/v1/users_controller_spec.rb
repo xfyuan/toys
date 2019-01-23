a7ae2c0eb50cc62ec1c9a6ee396a17fcb943dfac
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-01-23 14:36:57 +0800

    add users#create and specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -14,4 +14,39 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(json_response[:email]).to eq @user.email
     end
   end
+
+  describe 'Post #create' do
+    context 'when created successfully' do
+      before :each do
+        @user_attributes = attributes_for :user
+        post :create, params: { user: @user_attributes }
+      end
+
+      it { should respond_with 201 }
+
+      it 'returns the user record just created' do
+        json_response = JSON.parse response.body, symbolize_names: true
+        expect(json_response[:email]).to eq @user_attributes[:email]
+      end
+    end
+
+    context 'when created failed' do
+      before :each do
+        @invalid_user_attributes = { password: '123456', password_confirmation: '123456' }
+        post :create, params: { user: @invalid_user_attributes }
+      end
+
+      it { should respond_with 422 }
+
+      it 'render errors json' do
+        json_response = JSON.parse response.body, symbolize_names: true
+        expect(json_response).to have_key(:errors)
+      end
+
+      it 'render errors json with details message' do
+        json_response = JSON.parse response.body, symbolize_names: true
+        expect(json_response[:errors][:email]).to include("can't be blank")
+      end
+    end
+  end
 end
