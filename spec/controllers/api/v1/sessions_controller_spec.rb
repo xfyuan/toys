942801c914ff1023da14399581a8c215cca00147
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-02-13 16:05:24 +0800

    refactor test by adding a request helper. add sessions controller

---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::SessionsController, type: :controller do
+  describe 'Post #create' do
+    before :each do
+      @user = create :user
+    end
+
+    context 'when the credentials are correct' do
+      before :each do
+        credentials = { email: @user.email, password: '12345678' }
+        post :create, params: { session: credentials }
+      end
+
+      it { should respond_with 200 }
+
+      it 'returns the user record corresponding to the given credentials' do
+        @user.reload
+        expect(json_response[:auth_token]).to eq @user.auth_token
+      end
+    end
+
+    context 'when the credentials are incorrect' do
+      before :each do
+        credentials = { email: @user.email, password: 'invalidpassword' }
+        post :create, params: { session: credentials }
+      end
+
+      it { should respond_with 422 }
+
+      it 'returns a json with error' do
+        expect(json_response[:errors]).to eq 'Invalid email or password'
+      end
+    end
+  end
+
+  describe 'Post #destroy' do
+    before :each do
+      @user = create :user
+      sign_in @user
+      delete :destroy, params: { id: @user.auth_token }
+    end
+
+    it { should respond_with 204 }
+  end
+end
