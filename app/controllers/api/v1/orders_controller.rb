20038da79dc581702d10272444f68c6d7ecf49c5
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-03-06 23:44:40 +0800

    add orders#show action

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::OrdersController < ApplicationController
   def index
     render json: OrderSerializer.new(current_user.orders)
   end
+
+  def show
+    render json:  OrderSerializer.new(current_user.orders.find params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     scope module: :v1 do
       resources :users, only: [:show, :create, :update, :destroy] do
         resources :toys, only: [:create, :update, :destroy]
-        resources :orders, only: [:index]
+        resources :orders, only: [:index, :show]
       end
       resources :sessions, only: [:create, :destroy]
       resources :toys, only: [:index, :show]
