d79fa3707c394734aa93dfca90ab0bc4e95481ea
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-03-06 23:26:24 +0800

    add order model, and the relationship with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_095911) do
+ActiveRecord::Schema.define(version: 2019_03_06_152253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "toys", force: :cascade do |t|
     t.string "title", default: ""
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2019_02_20_095911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
