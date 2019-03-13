6d09622c501888c46e92da221d15dbf817feef90
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2019-03-13 16:27:00 +0800

    update toy quantities after a placement is created

--- spec/factories/placements.rb
+++ spec/factories/placements.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :placement do
     order
     toy
+    quantity { 1 }
   end
 end
--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Placement, type: :model do
-  before :each do
-    @placement = build :placement
-  end
+  let(:placement) { build :placement }
 
-  subject { @placement }
+  subject { placement }
 
   it { should respond_to :order_id }
   it { should respond_to :toy_id }
@@ -13,4 +11,11 @@ RSpec.describe Placement, type: :model do
 
   it { should belong_to :order }
   it { should belong_to :toy }
+
+  describe '#decrement_toy_quantity!' do
+    it 'decrease the toy quantity by placement quantity' do
+      toy = placement.toy
+      expect { placement.decrement_toy_quantity! }.to change { toy.quantity }.by(-placement.quantity)
+    end
+  end
 end
